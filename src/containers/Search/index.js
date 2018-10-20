89a11a1e16dafcb68907eee205f002a09d5c2fc7
Grover Sundstrom <grover.a.sundstrom@gmail.com>
2018-10-20 10:18:32 -0400

    Test fetching in Search container

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
+const SWAPI_BASE = 'https://swapi.co/api/';
+
 class Search extends Component {
+
+  state = {
+    films: [],
+    people: [],
+    planets: [],
+    species: [],
+    starships: [],
+    vehicles: [],
+    searchValue: 'l',
+  }
+
+  componentDidMount() {
+    fetch(`${SWAPI_BASE}films/?search=l`)
+      .then(response => response.json())
+      .then(json => this.setState({films: json.results}));
+  }
+
   render() {
+    const { films } = this.state;
     return(
-      <p>Search container</p>
+      <div>
+        <p>Search container</p>
+        <p>films length: {films.length}</p>
+      </div>
     )
   }
 }
