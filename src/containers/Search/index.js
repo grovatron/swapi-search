0812c5d6369367c07382ec763f9cbc5bef50cf8b
Grover Sundstrom <grover.a.sundstrom@gmail.com>
2018-10-20 11:12:36 -0400

    Change Search to query all the endpoints

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -26,13 +26,44 @@ class Search extends Component {
       fetch(`${SWAPI_BASE}films/?search=${event.target.value}`)
         .then(response => response.json())
         .then(json => this.setState({films: json.results}));
+      fetch(`${SWAPI_BASE}people/?search=${event.target.value}`)
+        .then(response => response.json())
+        .then(json => this.setState({people: json.results}));
+      fetch(`${SWAPI_BASE}planets/?search=${event.target.value}`)
+        .then(response => response.json())
+        .then(json => this.setState({planets: json.results}));
+      fetch(`${SWAPI_BASE}species/?search=${event.target.value}`)
+        .then(response => response.json())
+        .then(json => this.setState({species: json.results}));
+      fetch(`${SWAPI_BASE}starships/?search=${event.target.value}`)
+        .then(response => response.json())
+        .then(json => this.setState({starships: json.results}));
+      fetch(`${SWAPI_BASE}vehicles/?search=${event.target.value}`)
+        .then(response => response.json())
+        .then(json => this.setState({vehicles: json.results}));
     } else {
-      this.setState({films: []});
+      this.setState({
+        films: [],
+        people: [],
+        planets: [],
+        species: [],
+        starships: [],
+        vehicles: [],
+      }
+    );
     }
   }
 
   render() {
-    const { films, searchValue } = this.state;
+    const {
+      films,
+      people,
+      planets,
+      species,
+      starships,
+      vehicles,
+      searchValue
+    } = this.state;
     return(
       <div>
         <p>Search container</p>
@@ -42,6 +73,11 @@ class Search extends Component {
           placeholder='Search films, people, planets, etc.'
           onChange={this.onSearchInputChange} />
         <p>films length: {films.length}</p>
+        <p>people length: {people.length}</p>
+        <p>planets length: {planets.length}</p>
+        <p>species length: {species.length}</p>
+        <p>starships length: {starships.length}</p>
+        <p>vehicles length: {vehicles.length}</p>
       </div>
     )
   }
