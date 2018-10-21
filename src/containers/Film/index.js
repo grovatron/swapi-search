084e9da6b899d7f32a20317f9214df1e907f781a
Grover Sundstrom <grover.a.sundstrom@gmail.com>
2018-10-21 08:27:01 -0400

    Add fetching and loading logic to Film container

--- src/containers/Film/index.js
+++ src/containers/Film/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import Loader from '../../components/Loader';
 
 class Film extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: null,
+    }
+  }
+
+  async componentDidMount() {
+    let characters = await Promise.all(this.props.location.state.characters.map(
+      url => fetch(url).then(response => response.json())
+      .then(json => json.name)));
+    this.setState({ characters });
+  }
+
   render() {
+    const { characters } = this.state;
     return (
-      <p>Film Container</p>
+      <div>
+        {characters === null && <Loader />}
+        {characters !== null && <p>{characters[0]}</p>}
+      </div>
     )
   }
 }
